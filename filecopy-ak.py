1dcadedd3cd8b9835acca4a68a316c12df5d2d5d
yzzordorex <aimee@barbariangroup.com>
2014-08-14 00:18:21 -0700

    Adds option to choose audio or video files

--- filecopy-ak.py
+++ filecopy-ak.py
@@ -17,8 +17,8 @@ sourcePath = "/Users/aimee/Downloads/test/"
 destinationPath = "/Users/aimee/Downloads/dest/"
 
 if os.path.exists( sourcePath ) != True : 
-	print "Could not find", sourcePath, "Quitting."
-	quit()
+    print "Could not find", sourcePath, "Quitting."
+    quit()
 
 if os.path.exists( destinationPath ) != True :
     print "Could not find", destinationPath, "Quitting."
@@ -28,15 +28,33 @@ filesInSourcePath = os.listdir( sourcePath )
 filesInDestinationPath = os.listdir( destinationPath )
 
 if filesInSourcePath == []:
-	print "Source directory contains no files. Quitting."
-	quit()
+    print "Source directory contains no files. Quitting."
+    quit()
 
-fileTypes = ".mp3",".ogg" #,".avi", ".mpg", ".mpeg", ".mov", ".mp4", ".mkv", ".m4v"
+audioFileExts = ".mp3",".ogg"
+videoFileExts = ".avi", ".mpg", ".mpeg", ".mov", ".mp4", ".mkv", ".m4v"
+
+while True:
+    userInput = raw_input("Copy [a]udio files, [v]ideo files, or [A]ll files? ")
+
+    if re.match('^[avA]$',userInput):
+        break
+    else:
+        print "Copy [a]udio files, [v]ideo files, or [A]ll files? "
+        continue
+
+if userInput == "a" :
+    fileTypes = audioFileExts
+elif userInput == "v" :
+    fileTypes = videoFileExts
+elif userInput == "A" :
+    fileTypes = audioFileExts + videoFileExts
+
 sourceFiles = []
 fileCount = 0
 for file in filesInSourcePath :
-	lowercaseFilename = file.lower()
-	if lowercaseFilename.endswith(fileTypes) :
+    lowercaseFilename = file.lower()
+    if lowercaseFilename.endswith(fileTypes) :
         sourceFiles.append(file)
         fileCount += 1
 
@@ -45,15 +63,15 @@ if fileCount >= 1 :
     print fileCount, infEng.plural("file", fileCount), "found in source folder.\nWould you like to copy now?"
     print "................................................"
     while True:
-        userInput = raw_input("Enter 'y' to continue or 'n' to quit.\n ")
+        userInput = raw_input("[c]opy or [q]uit?")
 
-        if re.match('^[yn]$',userInput):
+        if re.match('^[cq]$',userInput):
             break
         else:
-            print "Enter 'y' to continue or 'n' to quit."
+            print "Enter 'c' to copy or 'q' to quit."
             continue
 
-    if userInput == 'y':
+    if userInput == 'c':
         for sourceFile in sourceFiles:
             print "Checking destination for", sourceFile, "....."
             if sourceFile not in filesInDestinationPath :
