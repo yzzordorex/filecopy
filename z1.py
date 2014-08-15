1720f6ee0915edda104945b307f74928f6576b57
yzzordorex <aimee@barbariangroup.com>
2014-08-14 20:01:58 -0700

    added assignment

---
+++ z1.py
@@ -0,0 +1,20 @@
+import os
+
+sourceDir = "/Users/aimee/Downloads/test/"
+destinationDir = "/Users/aimee/Downloads/dest/"
+
+
+# Rewrite the following as a function. 
+# The function should accept one argument, a path to a directory
+# It should return true if the directory exists
+# It should return false if the directory does not exist
+# There will be a part two....
+
+if os.path.exists( sourceDir ) != True : 
+    print "Could not find", sourceDir, "Quitting."
+    quit()
+
+if os.path.exists( destinationDir ) != True :
+    print "Could not find", destinationDir, "Quitting."
+    quit()
+
