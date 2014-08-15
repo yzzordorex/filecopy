ba5ef7580ca3179928e0169f53536e3e3bdf5661
yzzordorex <aimee@barbariangroup.com>
2014-08-14 20:16:23 -0700

    update to assign

--- z1.py
+++ z1.py
@@ -8,6 +8,8 @@ destinationDir = "/Users/aimee/Downloads/dest/"
 # The function should accept one argument, a path to a directory
 # It should return true if the directory exists
 # It should return false if the directory does not exist
+# test for the above two defined directories
+# bonus points if you add more directories to test and demonstrate their output
 # There will be a part two....
 
 if os.path.exists( sourceDir ) != True : 
