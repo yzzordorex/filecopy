21f1397fec975b63f5be9cd35189d069acf14853
retoid <retoid@gmail.com#>
2014-08-15 16:41:23 -0700

    reply 01

--- z1.py
+++ z1.py
@@ -1,7 +1,9 @@
 import os
 
+usersDir = "/Users/"
 sourceDir = "/Users/aimee/Downloads/test/"
 destinationDir = "/Users/aimee/Downloads/dest/"
+fakeDir = "/Users/SteveJobs/Secret/uber_secret/pc-BAK/My Documents/My Pictures/Steve/pr0n/"
 
 
 # Rewrite the following as a function. 
@@ -12,11 +14,16 @@ destinationDir = "/Users/aimee/Downloads/dest/"
 # bonus points if you add more directories to test and demonstrate their output
 # There will be a part two....
 
-if os.path.exists( sourceDir ) != True : 
-    print "Could not find", sourceDir, "Quitting."
-    quit()
+def pathCheck(p):
+    if os.path.exists( p ) != True : 
+		print "Could not find", p, "Quitting."
+		quit()
 
-if os.path.exists( destinationDir ) != True :
-    print "Could not find", destinationDir, "Quitting."
-    quit()
+#if os.path.exists( destinationDir ) != True :
+#   print "Could not find", destinationDir, "Quitting."
+#    quit()
 
+pathCheck(usersDir)
+pathCheck(sourceDir)
+pathCheck(destinationDir)
+pathcheck(fakeDir)
