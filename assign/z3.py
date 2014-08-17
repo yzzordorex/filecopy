f4395b619b5f460aa2c6bb4785173c691e9afa1f
yzzordorex <aimee@barbariangroup.com>
2014-08-17 09:42:17 -0700

    reorg, adding assignment 3

---
+++ assign/z3.py
@@ -0,0 +1,14 @@
+# 3. Using regular expressions, perform the following
+
+myString = "The quick brown fox jumped over the lazy dog."
+
+# a. count the number of times the letter o appears
+print "There are ", count, "instances of the letter O";
+
+# b. replace "fox" and "dog" with "cat".
+print catString
+
+# c. capitalize the first word of every sentence.
+myParagraph = "lorem ipsum dolor sit amet, consectetur adipiscing elit. etiam pretium felis felis, ut sollicitudin enim aliquet placerat. curabitur a malesuada quam. praesent scelerisque purus quis pretium mollis. pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. nulla hendrerit purus vel cursus viverra. phasellus auctor metus a erat condimentum, non pharetra ipsum hendrerit. sed pharetra a sapien nec imperdiet. nulla fringilla lacus eu ligula venenatis, sit amet volutpat sem commodo. quisque mattis est est, eu pellentesque mi egestas vel. donec vestibulum risus ac velit ultricies euismod. morbi volutpat mattis facilisis. vestibulum tristique pharetra malesuada."
+
+print myCapitalizedParagraph
