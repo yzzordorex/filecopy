b13b049c951f010d8d8aa4c5169790a89a50d0d8
yzzordorex <aimee@barbariangroup.com>
2014-08-17 08:39:28 -0700

    Adds assign 2

---
+++ z2.py
@@ -0,0 +1,5 @@
+# 2. Prompt for user input
+#   Create a new directory in user's home directory with name specified by input
+#   Create a new file in the directory
+#   Write to the file, listing all files/folders in user's home directory that are less than 1 day old
+#   
